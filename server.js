85115f5c1a99dcbb28d019a7803a00db1a2fe933
Philip Mlaker <shuphm@US.Int.SonicHealthcare>
2017-07-17 12:13:51 -0500

    heroku

--- server.js
+++ server.js
@@ -189,9 +189,9 @@ app.post("/submit", function(req, res) {
 
 
 
-// Set the app to listen on port 3000
-// app.listen(process.env.PORT || 3000);
+Set the app to listen on port 3000
+app.listen(process.env.PORT || 3000);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// });
