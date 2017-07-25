93dfb1df80b9b9b3158bc94af468d88d86d60c8d
Philip Mlaker <shuphm@US.Int.SonicHealthcare>
2017-07-25 12:01:34 -0500

    Help from Antonio!

--- server.js
+++ server.js
@@ -3,156 +3,82 @@
  * ========================= */
 
 // Dependencies
-
+var mongoose = require('mongoose');
 var express = require("express");
 var mongojs = require("mongojs");
 var logger = require("morgan");
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser());
+var mongodb = require('mongodb');
 
-
-var uri = 'mongodb://USERNAME:PASSWORD@ds157702.mlab.com:57702/heroku_r45mjcns';
-
-
-
-
+var URI = 'mongodb://pmlaker:Megan2305@ds157702.mlab.com:57702/heroku_r45mjcns';
+// var URI = 'mongodb://bigmongo:d33znutz@ds155160.mlab.com:55160/whygoalone2';
+mongodb.MongoClient.connect(URI, function(err, db) {
+  console.log("db connected");
+  
+  if(err) throw err;
 
 // Initialize Express
 
+var collection = db.collection('myCollection');
 
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));
 
 // Database configuration
 // Save the URL of our database as well as the name of our collection
-var databaseUrl = "mydb";
-var collections = ["myCollection"];
+// var databaseUrl = "mydb";
+// var collections = ["myCollection"];
 
-var databaseUrl2 = "testMenu";
-var collections2 = ["allTests"];
+// var databaseUrl2 = "testMenu";
+// var collections2 = ["allTests"];
 
 // Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
-var db2 = mongojs(databaseUrl2, collections2);
+// var db = mongojs(databaseUrl, collections);
+// var db2 = mongojs(databaseUrl2, collections2);
 
 // This makes sure that any errors are logged if mongodb runs into an issue
 // db.on("error", function(error) {
 //   console.log("Database Error:", error);
 // });
 
+  // app.get("/find/dept/:id", function(req, res) {
+  // var test_id = req.param('id');
+  // console.log()
 
-// Routes
-// 1. At the root path, send a simple hello world message to the browser
-app.get("/", function(req, res) {
-  
-   res.sendfile('public/home.html');
+  // collection.find({ $or: [ { "WebsiteCategory": test_id }, { "WebsiteCategory2": test_id } ] }, function(error, found) {
+  //   // Log any errors if the server encounters one
+  //   console.log("Finding ID" + test_id);
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  //   // Otherwise, send the result of this query to the browser
+  //   else {
+  //     res.json(found);
+  //     console.log("From server");
+  //     console.log(found);
+      
+  //   }
+  // });
+  // });
+//gage test:
+app.get("/find/dept/:id", function(req, res) {
+  var test_id = req.param('id');
+  db.collection("myCollection").find({$or:[{"WebsiteCategory": test_id }, { "WebsiteCategory2": test_id }]}).toArray(function(err, docs) {
+    console.log(docs);
+    console.log("found docs")
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
-
-// 2. At the "/all" path, display every entry in the animals collection
-app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.myCollection.find({}, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log(found);
-    }
-  });
-});
-
-app.get("/find2/:id", function(req, res) {
-  var test_id2 = req.param('id');
-
-
-
-
-
-  db2.allTests.find({"TestName": {$regex:test_id2, $options: 'i'} }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
-      
-    }
-  });
-  });
-
-
-
-
-
-
-// 3. At the "/name" path, display every entry in the animals collection, sorted by name
-app.get("/find/:id", function(req, res) {
-  var test_id = req.param('id');
-
-  db2.allTests.find({"Test": test_id }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
-      
-    }
-  });
-  });
-
-  app.get("/find/dept/:id", function(req, res) {
-  var test_id = req.param('id');
-
-  db.myCollection.find({ $or: [ { "WebsiteCategory": test_id }, { "WebsiteCategory2": test_id } ] }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
-      
-    }
-  });
-  });
-
-
-  app.get("/find/dept2/:id", function(req, res) {
-  var test_id = req.param('id');
-
-  db.myCollection.find({"TestSet": test_id }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
-      
-    }
-  });
-  });
-
-
-
-
 
 // Set the app to listen on port 3000
 app.listen(process.env.PORT || 3000);
 
 
+
+});
