2d9836b4962296bbb362cded0e1997600fb4889c
Philip Mlaker <shuphm@US.Int.SonicHealthcare>
2017-07-24 19:06:10 -0500

    heroku

--- server.js
+++ server.js
@@ -10,23 +10,17 @@ var logger = require("morgan");
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser());
-var connect = require('connect'), 
-mongo = require('mongodb');
+var mongo = require('mongodb');
 
 var URI = 'mongodb://pmlaker:Megan2305@ds157702.mlab.com:57702/heroku_r45mjcns';
 
-mongo.connect(URI, {}, function(error, db){
-
-  // console.log will write to the heroku log which can be accessed via the 
-  // command line as "heroku logs"
-  db.addListener("error", function(error){
-    console.log("Error connecting to MongoLab");
-  });
-
-
+mongodb.MongoClient.connect(URI, function(err, db) {
+  
+  if(err) throw err;
 
 // Initialize Express
 
+var collection = db.collection('myCollection');
 
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));
@@ -51,77 +45,77 @@ app.use(express.static("public"));
 
 // Routes
 // 1. At the root path, send a simple hello world message to the browser
-app.get("/", function(req, res) {
+// app.get("/", function(req, res) {
   
-   res.sendfile('public/home.html');
-});
+//    res.sendfile('public/home.html');
+// });
 
-// 2. At the "/all" path, display every entry in the animals collection
-app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.myCollection.find({}, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log(found);
-    }
-  });
-});
+// // 2. At the "/all" path, display every entry in the animals collection
+// app.get("/all", function(req, res) {
+//   // Query: In our database, go to the animals collection, then "find" everything
+//   db.myCollection.find({}, function(error, found) {
+//     // Log any errors if the server encounters one
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the result of this query to the browser
+//     else {
+//       res.json(found);
+//       console.log(found);
+//     }
+//   });
+// });
 
-app.get("/find2/:id", function(req, res) {
-  var test_id2 = req.param('id');
-
-
+// app.get("/find2/:id", function(req, res) {
+//   var test_id2 = req.param('id');
 
 
 
-  db.allTests.find({"TestName": {$regex:test_id2, $options: 'i'} }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
+
+
+//   db.allTests.find({"TestName": {$regex:test_id2, $options: 'i'} }, function(error, found) {
+//     // Log any errors if the server encounters one
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the result of this query to the browser
+//     else {
+//       res.json(found);
+//       console.log("From server");
+//       console.log(found);
       
-    }
-  });
-  });
+//     }
+//   });
+//   });
 
 
 
 
 
 
-// 3. At the "/name" path, display every entry in the animals collection, sorted by name
-app.get("/find/:id", function(req, res) {
-  var test_id = req.param('id');
+// // 3. At the "/name" path, display every entry in the animals collection, sorted by name
+// app.get("/find/:id", function(req, res) {
+//   var test_id = req.param('id');
 
-  db.allTests.find({"Test": test_id }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
+//   db.allTests.find({"Test": test_id }, function(error, found) {
+//     // Log any errors if the server encounters one
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the result of this query to the browser
+//     else {
+//       res.json(found);
+//       console.log("From server");
+//       console.log(found);
       
-    }
-  });
-  });
+//     }
+//   });
+//   });
 
   app.get("/find/dept/:id", function(req, res) {
   var test_id = req.param('id');
 
-  db.myCollection.find({ $or: [ { "WebsiteCategory": test_id }, { "WebsiteCategory2": test_id } ] }, function(error, found) {
+  collection.find({ $or: [ { "WebsiteCategory": test_id }, { "WebsiteCategory2": test_id } ] }, function(error, found) {
     // Log any errors if the server encounters one
     if (error) {
       console.log(error);
@@ -137,67 +131,28 @@ app.get("/find/:id", function(req, res) {
   });
 
 
-  app.get("/find/dept2/:id", function(req, res) {
-  var test_id = req.param('id');
+  // app.get("/find/dept2/:id", function(req, res) {
+  // var test_id = req.param('id');
 
-  db.myCollection.find({"TestSet": test_id }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-      console.log("From server");
-      console.log(found);
+  // db.myCollection.find({"TestSet": test_id }, function(error, found) {
+  //   // Log any errors if the server encounters one
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  //   // Otherwise, send the result of this query to the browser
+  //   else {
+  //     res.json(found);
+  //     console.log("From server");
+  //     console.log(found);
       
-    }
-  });
-  });
+  //   }
+  // });
+  // });
 
 
-// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
-app.get("/weight", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything,
-  // but this time, sort it by weight (-1 means descending order)
- db.animals.find().sort({ weight: -1 }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
-  });
-});
 
 
-app.post("/submit", function(req, res) {
 
-  // Save the request body as an object called book
-  var firstname = JSON.stringify(req.body);
-  var book = req.body;
-
-  console.log("This is from server " + firstname);
-
-  // If we want the object to have a boolean value of false,
-  // we have to do it here, because the ajax post will convert it
-  // to a string instead of a boolean
- 
-
-  // Save the book object as an entry into the books collection in mongo
-  db.test.save(book, function(error, saved) {
-    // Show any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the response to the client (for AJAX success function)
-    else {
-      res.send(saved);
-    }
-  });
-});
 
 
 
